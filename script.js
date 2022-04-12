99fa26f7c8ca268aa4a42cbb084cf428dc1cebcb
carolinehallersjo <103030403+carolinehallersjo@users.noreply.github.com>
2022-04-12 12:19:39 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,70 @@
+// Convert time to a format of hours, minutes, seconds, and milliseconds
+
+function timeToString(time) {
+  let diffInHrs = time / 3600000;
+  let hh = Math.floor(diffInHrs);
+
+  let diffInMin = (diffInHrs - hh) * 60;
+  let mm = Math.floor(diffInMin);
+
+  let diffInSec = (diffInMin - mm) * 60; //Här ändrade jag från 60, då jag inte vill att minuter ska synas.
+  let ss = Math.floor(diffInSec);
+
+  let diffInMs = (diffInSec - ss) * 100;
+  let ms = Math.floor(diffInMs);
+
+  //   let formattedMM = mm.toString().padStart(2, "0");
+  let formattedSS = ss.toString().padStart(2, "0");
+  let formattedMS = ms.toString().padStart(2, "0");
+
+  return `${formattedSS}:${formattedMS}`;
+  // return `${formattedMM}:${formattedSS}:${formattedMS}`;
+}
+
+// Declare variables to use in our functions below
+
+let startTime;
+let elapsedTime = 0;
+let timerInterval;
+
+// Create function to modify innerHTML
+
+function print(txt) {
+  document.getElementById("display").innerHTML = txt;
+}
+
+// Create "start", "pause" and "reset" functions
+
+function start() {
+  document.body.classList.add("timer-started")
+  startTime = Date.now() - elapsedTime;
+  timerInterval = setInterval(function printTime() {
+    elapsedTime = Date.now() - startTime;
+    print(timeToString(elapsedTime));
+  }, 10);
+  showButton("PAUSE");
+}
+
+function pause() {
+  document.body.classList.remove("timer-started")
+  document.body.classList.add("timer-stop")
+  clearInterval(timerInterval);
+  showButton(""); //Här tog jag bort "PLAY" för att jag inte vill att playknappen ska dyka upp igen
+}
+
+// Create function to display buttons
+
+function showButton(buttonKey) {
+  const buttonToShow = buttonKey === "PLAY" ? playButton : pauseButton;
+  const buttonToHide = buttonKey === "PLAY" ? pauseButton : playButton;
+  buttonToShow.style.display = "block";
+  buttonToHide.style.display = "none";
+}
+// Create event listeners
+
+let playButton = document.getElementById("playButton");
+let pauseButton = document.getElementById("pauseButton");
+
+playButton.addEventListener("click", start);
+pauseButton.addEventListener("click", pause);
+
